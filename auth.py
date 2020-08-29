5c89a529b98dcbb34a222324b79b6ec1d6454ced
AnonymousOfficialUser <shashanksi2005@gmail.com>
2020-08-29 20:50:37 +0530

    Create auth.py
    Keys for the main program

---
+++ auth.py
@@ -0,0 +1,6 @@
+#Keys from Twitter Developer Account
+
+consumer_key        = 'ABCDEFGHIJKLKMNOPQRSTUVWXYZ'
+consumer_secret     = '1234567890ABCDEFGHIJKLMNOPQRSTUVXYZ'
+access_token        = 'ZYXWVUTSRQPONMLKJIHFEDCBA'
+access_token_secret = '0987654321ZYXWVUTSRQPONMLKJIHFEDCBA'
